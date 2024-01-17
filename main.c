ea190454c6aecf4ad4f51c7882e240b61014233c
mandreidan <155060371+mandreidan@users.noreply.github.com>
2024-01-17 21:54:49 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,97 +1,101 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-char spanzuratoare[40][40];
-void model_spanzuratoarea(char v[40][40])
+#define dimensiune_matrice 40
+#define lungime_maxima_cuvant 100
+
+void model_spanzuratoarea(char spanzuratoare[dimensiune_matrice][dimensiune_matrice])
 {
     for(int i=1;i<=10;i++)
     {
-        v[14][i]='_';
+        spanzuratoare[14][i]='_';
     }
     for(int i=2;i<=14;i++)
     {
-        v[i][3]='|';
-        v[i][8]='|';
+        spanzuratoare[i][3]='|';
+        spanzuratoare[i][8]='|';
     }
     for(int i=3;i<=29;i++)
     {
-        v[1][i]='_';
+        spanzuratoare[1][i]='_';
     }
     for(int i=8;i<=26;i++)
     {
-        v[2][i]='_';
+        spanzuratoare[2][i]='_';
     }
-    v[3][26]='|';
-    v[2][29]='|';
-    v[3][29]='|';
+    spanzuratoare[3][26]='|';
+    spanzuratoare[2][29]='|';
+    spanzuratoare[3][29]='|';
 }
-void deseneaza(int n,char v[40][40])
+void deseneaza(int n,char spanzuratoare[dimensiune_matrice][dimensiune_matrice])
 {
     if(n==1)
     {
-        v[3][25]=v[3][27]=v[3][28]=v[3][30]='_';
-        v[4][24]=v[5][24]='|';
-        v[4][31]=v[5][31]='|';
-        for(int i=25;i<=30;i++) v[5][i]='_';
+        spanzuratoare[3][25]=spanzuratoare[3][27]=spanzuratoare[3][28]=spanzuratoare[3][30]='_';
+        spanzuratoare[4][24]=spanzuratoare[5][24]='|';
+        spanzuratoare[4][31]=spanzuratoare[5][31]='|';
+        for(int i=25;i<=30;i++) spanzuratoare[5][i]='_';
     }
     if(n==2)
     {
-        v[6][26]=v[6][29]='|';
+        spanzuratoare[6][26]=spanzuratoare[6][29]='|';
     }
     if(n==3)
     {
         for(int i=7;i<=9;i++)
         {
-            v[i][24]='|';
-            v[i][31]='|';
+            spanzuratoare[i][24]='|';
+            spanzuratoare[i][31]='|';
         }
         for(int i=25;i<=30;i++)
         {
-            v[9][i]='_';
+            spanzuratoare[9][i]='_';
         }
     }
     if(n==4)
     {
         for(int i=20;i<=23;i++)
         {
-            v[6][i]=v[7][i]='_';
+            spanzuratoare[6][i]=spanzuratoare[7][i]='_';
         }
-        v[7][19]='|';
+        spanzuratoare[7][19]='|';
     }
     if(n==5)
     {
         for(int i=32;i<=35;i++)
         {
-            v[6][i]=v[7][i]='_';
+            spanzuratoare[6][i]=spanzuratoare[7][i]='_';
         }
-        v[7][36]='|';
+        spanzuratoare[7][36]='|';
     }
     if(n==6)
     {
         for(int i=10;i<=11;i++)
         {
-            v[i][24]='|';
-            v[i][26]='|';
+            spanzuratoare[i][24]='|';
+            spanzuratoare[i][26]='|';
         }
-        v[11][25]='_';
+        spanzuratoare[11][25]='_';
     }
     if(n==7)
     {
         for(int i=10;i<=11;i++)
         {
-            v[i][29]='|';
-            v[i][31]='|';
+            spanzuratoare[i][29]='|';
+            spanzuratoare[i][31]='|';
         }
-        v[11][30]='_';
+        spanzuratoare[11][30]='_';
     }
 }
-void afisare(char v[40][40])
+void afisare(char spanzuratoare[40][40])
 {
     for(int i=0;i<20;i++)
     {
         for(int j=0;j<40;j++)
         {
-            printf("%c",v[i][j]);
+            printf("%c",spanzuratoare[i][j]);
         }
         printf("\n");
     }
@@ -99,25 +103,29 @@ void afisare(char v[40][40])
 
 int main() {
 
-    char cuvant[100];
-    printf("Bun venit la Spanzuratoarea\nScrieti ghicitoarea care trebuie gasita de prietenii tai: ");
-    int nr_cuvinte=0;
-    for (int i = 0; i < 100; i++) {
-        scanf ("%c", &cuvant[i]);
-        if(cuvant[i]==' ') nr_cuvinte++;
-        if (cuvant[i] == 10)
-        {
-            nr_cuvinte++;
-            cuvant[i] = '\0';
-            break;
-        }
+    char spanzuratoare[dimensiune_matrice][dimensiune_matrice]={0};
+    char cuvant[lungime_maxima_cuvant];
+    printf("Bun venit la Spanzuratoarea\n");
+
+    FILE *pf= fopen("C:\\Users\\Lenovo\\Desktop\\text.txt","r");
+
+    srand(time(NULL));
+    int random_variable = rand()%10000;
+    for(int i=0;i<random_variable;i++)
+    {
+
+        fgets(cuvant ,lungime_maxima_cuvant,pf);
+
     }
 
-    if(nr_cuvinte>1) printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nGhicitoarea este din %d cuvinte\n\n", nr_cuvinte);
-    else
-    {
-        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nGhicitoarea este dintr-un cuvant cu %llu litere \n", strlen(cuvant));
-    }
+    fclose(pf);
+
+
+
+
+
+    printf("Cuvantul este din %llu litere \n", strlen(cuvant)-1);
+
     char ghicitoare[100];
     strcpy(ghicitoare,cuvant);
     for(int i=0;i<strlen(cuvant);i++)
